8159e74d5f5d561b8630bf10137a792710964125
Alewitox <ale.limi95@gmail.com>
2019-12-05 12:52:53 +0000

    Reestructuración completa del header de activity_tab_layout

--- app/src/main/java/com/example/series/SerieAdapter.kt
+++ app/src/main/java/com/example/series/SerieAdapter.kt
@@ -3,9 +3,10 @@ package com.example.series
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.series.authentication.model.Serie
+
 
 
 class SeriesAdapter(var series: ArrayList<Series>) :
