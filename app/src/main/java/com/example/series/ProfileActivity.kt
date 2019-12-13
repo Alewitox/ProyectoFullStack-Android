3f7218260c5f887429cfa30cf4cb06b7db7406fa
Alewitox <ale.limi95@gmail.com>
2019-12-13 19:13:42 +0000

    Dialog añadido en el la actividad perfil

--- app/src/main/java/com/example/series/ProfileActivity.kt
+++ app/src/main/java/com/example/series/ProfileActivity.kt
@@ -2,7 +2,13 @@ package com.example.series
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_dialog.*
+import kotlinx.android.synthetic.main.activity_dialog.view.*
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.toolbar
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -11,7 +17,29 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         setSupportActionBar(toolbar)
+
+        editAliasBtn.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.activity_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Editar alias")
+
+            val mAlertDialog = mBuilder.show()
+            mDialogView.dialogButtonAccept.setOnClickListener{
+                mAlertDialog.dismiss()
+                val name = mAlertDialog.dialogAlias.text.toString()
+                aliasTextView.setText(name)
+                profileTextView.setText(name)
+
+            }
+            mDialogView.dialogButtonCancel.setOnClickListener{
+                mAlertDialog.dismiss()
+            }
+        }
     }
+
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
